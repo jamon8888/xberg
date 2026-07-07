6125725f867bb9495d3e4dc295ae0b36a752194f
Your Name <user@example.com>
2026-07-07 12:18:20 +0200

    chore(wasm-runtime): scaffold TypeScript project

---
+++ packages/xberg-wasm-runtime/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./embedder";
+export * from "./store";
+export * from "./ner";
+export * from "./ocr";
+export * from "./cache";
+export { createXbergRuntimeFactory } from "./factory";
