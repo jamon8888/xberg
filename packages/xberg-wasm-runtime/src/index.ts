4424961cd248fcce2814d05d3ed718415d17abe6
Your Name <user@example.com>
2026-07-07 14:12:31 +0200

    feat(wasm-runtime): async_shim.ts single-flight enforcement

--- packages/xberg-wasm-runtime/src/index.ts
+++ packages/xberg-wasm-runtime/src/index.ts
@@ -3,4 +3,5 @@ export * from "./store";
 export * from "./ner";
 export * from "./ocr";
 export * from "./cache";
+export * from "./async_shim";
 export { createXbergRuntimeFactory } from "./factory";
