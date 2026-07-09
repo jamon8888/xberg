767e407cb1f4c10916aca89f9ddd1e3db710eb43
Your Name <user@example.com>
2026-07-09 16:19:59 +0200

    feat(wasm-runtime): add shared host-agnostic ingestFolder orchestrator

--- packages/xberg-wasm-runtime/src/index.ts
+++ packages/xberg-wasm-runtime/src/index.ts
@@ -4,4 +4,5 @@ export * from "./ner.js";
 export * from "./ocr.js";
 export * from "./cache.js";
 export * from "./async_shim.js";
+export * from "./ingest-folder.js";
 export { createXbergRuntimeFactory } from "./factory.js";
